66b50a417ffcdb0386d91d54d5748ca2faf8423f
milesba4 <milesbaffour@gmail.com>
2022-06-15 14:20:32 -0400

    passing navLinks prop into navBar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import UserProfile from "./components/UserProfile/UserProfile"
 import Advertisements from "./components/Advertisements/Advertisements"
 import { codepathUserProfile, firstTweet, navLinks } from "./constants"
 
+
 export default function App() {
   return (
     <div className="app">
-      <Navbar />
+      <Navbar navLinks={navLinks} />
       <main>
         <UserProfile />
         <Feed />
