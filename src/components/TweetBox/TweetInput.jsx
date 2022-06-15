3505adb4e78bc04b1d9a8bedf2a748a7a64c116a
milesba4 <milesbaffour@gmail.com>
2022-06-15 17:50:06 -0400

    equating value prop and on changeProp in text area element

--- src/components/TweetBox/TweetInput.jsx
+++ src/components/TweetBox/TweetInput.jsx
@@ -6,7 +6,7 @@ export default function TweetInput({handleOnChange, value}) {
     <div className="tweet-textarea">
       <AvatarIcon />
 
-      <textarea name="new-tweet-input" type="text" placeholder="What's Happening?"></textarea>
+      <textarea name="new-tweet-input" type="text" placeholder="What's Happening?" value={value} onChange ={handleOnChange}></textarea>
 
       <SmileIcon />
     </div>
